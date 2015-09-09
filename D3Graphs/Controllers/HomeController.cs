11c9fcaa70ae79053d7b26629d20dfbdb623f3cc
karthikraobr <karthik.0703@gmail.com>
2015-09-09 22:15:44 +0530

    Initial commit

---
+++ D3Graphs/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using D3Graphs.Models;
+
+namespace D3Graphs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees(int? count)
+        {
+            if (count == null) count = 15000;
+            if (count > 50000) count = 50000;
+
+            IList<EmployeeModel> _comments = new List<EmployeeModel>();
+            Random r = new Random();
+            int rInt = r.Next(0, 100); //for ints
+            for (int i = 1; i <= count; i++)
+            {
+                _comments.Add(new EmployeeModel
+                {
+                    Id = i,
+                    Name = "John Doe Number " + i,
+                    Department = r.Next(1, 4)
+                });
+            }
+
+            return new JsonResult()
+            {
+                Data = _comments,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                MaxJsonLength = Int32.MaxValue
+            };
+        }
+    }
+}
---
+++ D3Graphs/Models/EmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace D3Graphs.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public int Department { get; set; }
+        public string Name { get; set; }
+    }
+}
